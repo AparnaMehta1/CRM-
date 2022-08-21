8ea63f36da38f82b59cbb0f2a777ec860fa77c2a
Anurag <anuragsinha120@gmail.com>
2022-08-21 21:21:08 +0530

    21Aug

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DropdownButton, Dropdown } from "react-bootstrap";
 import { userSignup, userSignin } from "../api/auth";
 import { useNavigate } from 'react-router-dom';
@@ -8,12 +8,11 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import userIcon from '../Styles/user.svg'
-import {GoogleLogin, GoogleLogout} from 'react-google-login'
 import '../Styles/Login.css';
 
 function Login() {
   const [signUp, setshowsignUp] = useState(false);
-  const [userType, setuserType] = useState("CUSTOMER");
+  const [userType, setuserType] = useState("SELECT");
   const [userSignupData, setUserSignupData] = useState({});
   const [message, setMessage] = useState("");
 
@@ -21,16 +20,29 @@ function Login() {
     setshowsignUp(!signUp);
   };
 
-  //here we are changing the state for userTypes on select
-  const handleSelect = (e) => {
-    setuserType(e);
-  };
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+        const userTypes = localStorage.getItem("userTypes");
+        redirectToHomePage(userTypes);
+    }
+}, []);
+
+  const history = useNavigate();  
+  const redirectToHomePage = (userTypes)=>{
+    if (userTypes === "CUSTOMER") {
+      history("/customer");
+    } else if (userTypes === "ENGINEER") {
+      history("/engineer");
+    } else {
+      history("/admin1");
+    }
+  }
 
   const updateSignupData = (e) => {
     userSignupData[e.target.id] = e.target.value;
     console.log(userSignupData);
   };
-  //we are grabing the input value with id and storing it an object
+  //we are grabing the input value with id and storing it in an object
   const signupFn = (e) => {
     const username = userSignupData.username;
     const userId = userSignupData.userId;
@@ -47,22 +59,21 @@ function Login() {
     console.log("DATA", data);
     e.preventDefault();
 
-    //what is the data here
     userSignup(data)
       .then(function (response) {
-        if (response === 201) {
-          history(0);
+        if (response === 201){
+          history("/");
         }
       })
-      .catch(function (error) {
-        if (error.response.status === 400) {
+      .catch(function (error){
+        if (error.response.status === 400){
           setMessage(error.response.data.message);
-        } else {
+        }else{
           console.log(error);
         }
       });
   };
-  const history = useNavigate();
+  
 
   const loginFn = (e) => {
     const userId = userSignupData.userId;
@@ -79,22 +90,27 @@ function Login() {
       .then(function (response) {
         console.log(response);
         if (response.status === 200) {
-          //why we are setting the data in localStorage in login fn??
-          //userId, email, userType, userStatis, token
-          localStorage.setItem("name", response.data.name);
-          localStorage.setItem("userId", response.data.userId);
-          localStorage.setItem("email", response.data.email);
-          localStorage.setItem("userTypes", response.data.userTypes);
-          localStorage.setItem("userStatus", response.data.userStatus);
-          //what is token??
-          localStorage.setItem("token", response.data.accessToken);
+          const {
+            accessToken,
+            email,
+            name,
+            userId,
+            userStatus,
+            userTypes,
+            message,
+        } = response.data;
 
-          if (response.data.userTypes === "CUSTOMER") {
-            history("/customer");
-          } else if (response.data.userTypes === "ENGINEER") {
-            history("/engineer");
-          } else {
-            history("/admin");
+        if(message){
+          alert(message)
+        }else{
+          localStorage.setItem("name", name);
+          localStorage.setItem("userId", userId);
+          localStorage.setItem("email", email);
+          localStorage.setItem("userTypes", userTypes);
+          localStorage.setItem("userStatus", userStatus);
+          localStorage.setItem("token", accessToken);
+
+          redirectToHomePage(userTypes)
           }
         }
       })
@@ -107,9 +123,6 @@ function Login() {
       });
   };
 
-  const gmailLogin=()=>{
-    window.location.href('https://accounts.google.com/ServiceLogin/signinchooser?service=mail&passive=1209600&osid=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
-  }
   return (
     <>
    <div className="backGround">
@@ -124,22 +137,22 @@ function Login() {
               <div className="login">
                 <form onSubmit={loginFn}>
                   <h4 className="text text-center p-3"> Login</h4>
-                  <input
-                    className="form_input m-2 form-control"
-                    placeholder="Enter Your User Id"
+                  <div><input
+                    className="form_input m-2 "
+                    placeholder="User Id"
                     type="text"
                     id="userId"
                     onChange={updateSignupData}
                     required
-                  />
-                  <input
-                    className="m-2 form-control"
+                  /></div>
+                  <div><input
+                    className="m-2 "
                     type="password"
                     id="password"
                     placeholder="Password"
                     onChange={updateSignupData}
                     required
-                  />
+                  /></div>
                   <button className="button fw-bolder m-2">
                     Login
                   </button>
@@ -152,7 +165,7 @@ function Login() {
                   <div className="text-warning text-center">{message}</div>
                 </form>
                 <div className="container mx-5">
-                <GoogleIcon className="social_icon" onClick={gmailLogin}/>
+                <GoogleIcon className="social_icon" />
                 <FacebookIcon className="social_icon"/>
                 <TwitterIcon className="social_icon"/>
                 <GitHubIcon className="social_icon"/>
@@ -163,7 +176,7 @@ function Login() {
                 <form onSubmit={signupFn}>
                   <h4 className="text text-center p-3"> Signup</h4>
                   <input
-                    className="input-group m-2 form-control"
+                    className="input-group m-2"
                     type="text"
                     placeholder="Enter your Name"
                     id="username"
@@ -171,7 +184,7 @@ function Login() {
                     required
                   />
                   <input
-                    className="input-group m-2 form-control"
+                    className="input-group m-2"
                     type="text"
                     placeholder="Enter your userId"
                     id="userId"
@@ -179,7 +192,7 @@ function Login() {
                     required
                   />
                   <input
-                    className="input-group m-2 form-control"
+                    className="input-group m-2"
                     type="email"
                     placeholder="Enter your email"
                     id="email"
@@ -187,28 +200,31 @@ function Login() {
                     required
                   />
                   <input
-                    className="input-group m-2 form-control"
+                    className="input-group m-2"
                     type="password"
                     placeholder="Enter Password"
                     id="password"
                     onChange={updateSignupData}
                     required
                   />
-                  <div className="input-group m-2 form-control bg-dark">
-                    <span className="text-light">User Type</span>
+                  <div className="dropdown m-2">
+                    <span className="usertype m-1">User Type</span>
                     <DropdownButton
                       align="end"
                       title={userType}
-                      variant="dark"
+                      variant="transparent"
                       className="mx-2"
-                      onSelect={handleSelect}
+                      onSelect={(e)=>setuserType(e)}
                     >
-                      <Dropdown.Item eventKey="CUSTOMER">
+                    <div className="dropdown_item"><Dropdown.Item eventKey="SELECT">
+                        SELECT
+                      </Dropdown.Item>
+                    <Dropdown.Item eventKey="CUSTOMER">
                         CUSTOMER
                       </Dropdown.Item>
                       <Dropdown.Item eventKey="ENGINEER">
                         ENGINEER
-                      </Dropdown.Item>
+                      </Dropdown.Item></div>
                     </DropdownButton>
                   </div>
                   <button className="button m-2 d-flex justify-content-center align-items-center fw-bolder">
@@ -222,6 +238,12 @@ function Login() {
                   </div>
                   <div className="text-warning text-center">{message}</div>
                 </form>
+                <div className="container mx-5">
+                <GoogleIcon className="social_icon" />
+                <FacebookIcon className="social_icon"/>
+                <TwitterIcon className="social_icon"/>
+                <GitHubIcon className="social_icon"/>
+                </div>
               </div>
             )}
           </div>
